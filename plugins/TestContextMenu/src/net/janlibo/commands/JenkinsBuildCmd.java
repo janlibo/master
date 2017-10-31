4d6ba63343b23ba14232809dfefff91f2f6472a6
Jan Libo <Jan.Libo@software.dell.com>
2017-10-31 09:30:35 +0100

    polished branch name

--- plugins/TestContextMenu/src/net/janlibo/commands/JenkinsBuildCmd.java
+++ plugins/TestContextMenu/src/net/janlibo/commands/JenkinsBuildCmd.java
@@ -1,8 +1,7 @@
 package net.janlibo.commands;
 
 import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 
@@ -11,12 +10,20 @@ import net.janlibo.dialogs.RunJobDialog;
 abstract class JenkinsBuildCmd extends AbstractHandler {
 
 	protected void executeBuild(final String branchName, final String commitName) {
-		System.out.println("Branch: " + branchName);
+		System.out.println("Branch: " + polishBranchName(branchName));
 		System.out.println("Commit: " + commitName);
 		Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
-		if (shell != null) {
-			new RunJobDialog(shell, branchName, commitName).open();
+		if (shell != null) {			
+			new RunJobDialog(shell, polishBranchName(branchName), commitName).open();
 		}
+	}
+	
+	private String polishBranchName(final String branchName) {
+		if (branchName.startsWith(Constants.R_HEADS)) {
+			return branchName.replaceFirst(Constants.R_HEADS, "");
+		}
+			
+		return branchName;
 	}
 
 }
